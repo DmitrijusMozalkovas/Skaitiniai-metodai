25947ac00d16aec9ebe748c3ed04aa454476b9dd
DmitrijusMozalkovas <dmitrijus.mozalkovas@leu.lt>
2013-09-23 11:12:07 +0200

    Pusiaukirtosmetodas papildytas

--- Nlsp/pusiaukirtosmetodas.m
+++ Nlsp/pusiaukirtosmetodas.m
@@ -3,8 +3,12 @@ function saknis = pusiaukirtosmetodas(func,x1,x2,filter,tol)
 % IVEDIMO PARAMETRAI:
 % filter = singuliarumo filtras: 0 = iðjungta (pagal nutylejima), 1 = ijungta.
 % tol = paklaida (pagal nutylejima 0.0001).
+% nargin = grazina ivesties argumentu, nustatytus funkcijos skaièiu.
+% Jeigu 
 if nargin < 5; tol = 0.0001; end
 if nargin < 4; filter = 0; end
+% 
+%
 f1 = func(x1);
 if f1 == 0.0; saknis = x1; return; end
 f2 = func(x2);
@@ -12,6 +16,7 @@ if f2 == 0.0; saknis = x2; return; end
 if f1*f2 > 0;
 error('Nurodytame intervale (x1,x2) nera funkcijos nulio')
 end
+% ceil = 
 n = ceil(log2(abs(x2 - x1)/tol));
 for i = 1:n
 x3 = 0.5*(x1 + x2);
